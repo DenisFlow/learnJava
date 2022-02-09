2de79fc70bc8b2f0e91b9f95ffb55eac43032397
DenisFlow <denhelloworld@gmail.com>
2022-02-09 08:30:59 +0300

    a1_hello_world 7. Primitive Types

---
+++ FirstSteps/src/a1_hello_world/Hello.java
@@ -0,0 +1,15 @@
+package a1_hello_world;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Denis");
+        int myFirstNumber = (10 + 5) + 2*10;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("my total " + myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+        System.out.println();
+    }
+}
