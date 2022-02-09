2b4182bdbdad2c84c2e401832c46f6d747a43681
DenisFlow <denhelloworld@gmail.com>
2022-02-09 16:02:33 +0300

    b3_1_Task

--- Java Tutorial Expressions_Statements_Code blocks_Methods and more/src/b2_StatementsWhiteSpacesAndIndenting/StatementsWhiteSpacesAndIndenting.java
+++ Java Tutorial Expressions_Statements_Code blocks_Methods and more/src/b2_StatementsWhiteSpacesAndIndenting/StatementsWhiteSpacesAndIndenting.java
@@ -1,7 +1,20 @@
-package b2_StatementsWhiteSpacesAndIndenting;
-
-public class StatementsWhiteSpacesAndIndenting {
-    public static void main(String[] args) {
-
-    }
-}
+//package b2_StatementsWhiteSpacesAndIndenting;
+//
+//public class StatementsWhiteSpacesAndIndenting {
+//    public static void main(String[] args) {
+//        int myVariable = 50;
+//        myVariable++;
+//        myVariable--;
+//        System.out.println("This is value" + myVariable);
+//
+//        System.out.println("I'm here" +
+//                " and you " +
+//                "here too!");
+//
+//        int anotherVariable = 50;
+//        myVariable--;
+//        System.out.println("another one");
+//
+//    }
+//}
+//// 4
---
+++ Java Tutorial Expressions_Statements_Code blocks_Methods and more/src/b3_IfKeywordsAndCodeBlocks/IfKeywordsAndCodeBlocks.java
@@ -0,0 +1,17 @@
+//package b3_IfKeywordsAndCodeBlocks;
+//
+//public class IfKeywordsAndCodeBlocks {
+//    public static void main(String[] args) {
+//        boolean gameOver = true;
+//        int score = 5000;
+//        int levelCompleted = 5;
+//        int bonus = 100;
+//        if (score == 5000)
+//            System.out.println("Your score was " + score);
+//        if (bonus == 100){
+//            boolean newOne = true;
+//        }
+//        newOne = false;
+//
+//    }
+//}
