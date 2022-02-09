75e8409807eaf63204b9194c15e0e371fa6a379f
DenisFlow <denhelloworld@gmail.com>
2022-02-09 12:50:29 +0300

    b2_StatementsWhiteSpacesAndIndenting

---
+++ Java Tutorial Expressions_Statements_Code blocks_Methods and more/src/b1_KeywordsAndExpressions/KeywordsAndExpressions.java
@@ -0,0 +1,13 @@
+//package a1_KeywordsAndExpressions;
+//
+//public class KeywordsAndExpressions {
+//    public static void main(String[] args) {
+//        int score = 100;
+//        if (score == 100) {
+//            System.out.println("You got the high score");
+//            score = 0;
+//        }
+//
+//
+//    }
+//}
---
+++ Java Tutorial Expressions_Statements_Code blocks_Methods and more/src/b2_StatementsWhiteSpacesAndIndenting/StatementsWhiteSpacesAndIndenting.java
@@ -0,0 +1,7 @@
+package b2_StatementsWhiteSpacesAndIndenting;
+
+public class StatementsWhiteSpacesAndIndenting {
+    public static void main(String[] args) {
+
+    }
+}
