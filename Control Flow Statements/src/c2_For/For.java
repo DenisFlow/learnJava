31ef24d28a5fd76d87e3b4325d396a19a171b776
DenisFlow <denhelloworld@gmail.com>
2022-02-10 21:03:54 +0300

    end c2_For

--- Control Flow Statements/src/c2_For/For.java
+++ Control Flow Statements/src/c2_For/For.java
@@ -2,6 +2,6 @@ package c2_For;
 
 public class For {
     public static void main(String[] args) {
-        
+        c2_1_Task.Task();
     }
 }
--- Control Flow Statements/src/c3_While/While.java
+++ Control Flow Statements/src/c3_While/While.java
@@ -2,6 +2,6 @@ package c3_While;
 
 public class While {
     public static void main(String[] args) {
-        
+
     }
 }
