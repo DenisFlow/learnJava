15156b02d6d16bfca231de10598e28a9db087c1a
DenisFlow <denhelloworld@gmail.com>
2022-02-10 22:25:44 +0300

    c3_1_Task

--- Control Flow Statements/src/c3_While/While.java
+++ Control Flow Statements/src/c3_While/While.java
@@ -2,6 +2,91 @@ package c3_While;
 
 public class While {
     public static void main(String[] args) {
+//        Task();
+//        DoLoop();
+//        Task2();
+//        Task3();
+        c3_1_Task.voidTask();
+    }
+    /*
+    Modify the while code above
+Make it also record the total
+number of even numbers it has found
+and break once 5 are found and at the end,
+display the total number of even numbers found
+    */
 
+    public static void Task3(){
+        int number = 4;
+        int finishNumber = 20;
+        int totalCount = 0;
+        int totalSum = 0;
+
+        while (number <= finishNumber){
+            number++;
+            if (isEvenNumber(number)) {
+                totalSum += number;
+                System.out.println("Even number " + number);
+                if (++totalCount >= 5)
+                    break;
+            }
+        }
+        System.out.println("Sum of even number " + totalSum);
+
+    }
+
+
+
+
+    /*
+    Create a method called isEvenNumber that takes a parameter of type int
+     Its purpose is to determine if the argument passed to the method is
+      an even number or not.
+       return true if an even number, otherwise return false,'|
+    */
+
+    public static void Task2(){
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number <= finishNumber){
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
+
+    }
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    public static void DoLoop(){
+        int count = 0;
+        do {
+            System.out.println(count++);
+        } while (count < 5);
+    }
+
+    public static void Task(){
+        int count = 0;
+        while ( count <= 5 ) {
+            System.out.println(count);
+            count++;
+        }
+        System.out.println("=====================================");
+        for (count = 0; count <= 5; count++){
+            System.out.println(count);
+        }
+
+        count = 0;
+        while (true) {
+            if (count >= 6)
+                break;
+            System.out.println(count);
+            count++;
+        }
     }
 }
---
+++ Control Flow Statements/src/c3_While/c3_1_Task.java
@@ -0,0 +1,27 @@
+package c3_While;
+
+public class c3_1_Task {
+    public static void voidTask(){
+        System.out.println(sumOfDigits(125));;
+    }
+/*
+Write a method with the name sumDigits that has one int parameter called number. If parameter is >= 10 then the method should process the number and return sum of all digits, otherwise return -1 to indicate an invalid value.
+The numbers from 0-9 have 1 digit so we don't want to process them, also we don't want to process negative numbers, so also return -1 for negative numbers.
+For example calling the method sumDigits(125) should return 8 since 1 +2 + 5 = 8.
+Calling the method sumDigits(l) should return -1 as per requirements described above.
+
+*/
+    public static int sumOfDigits(int number){
+        if (number <= 10) {
+            return -1;
+        } else {
+            int sum = 0;
+            while (number > 0) {
+                sum += number % 10;
+                number = number / 10;
+            }
+            return sum;
+        }
+
+    }
+}
