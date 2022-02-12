12f90f972045687732882c430a08668c7ade75a9
DenisFlow <denhelloworld@gmail.com>
2022-02-12 22:03:31 +0300

    d2_Override

---
+++ OOP Part 1 - Classes, Constructors and Inheritance/src/d2_Inheritance/Inheritance.java
@@ -0,0 +1,10 @@
+package d2_Inheritance;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        Dog dog = new Dog("You", 8, 20, 2, 4, 1, 20, "New");
+        dog.eat();
+
+    }
+}
